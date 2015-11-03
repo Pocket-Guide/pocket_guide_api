a9b08759e04cf705c216a618c5bfbed0cda440bf
山田　潤 <junyamada107@gmail.com>
2015-11-04 00:00:31 +0900

    set up user resource

--- app/controllers/tourists_controller.rb
+++ app/controllers/tourists_controller.rb
@@ -1,2 +1,29 @@
 class TouristsController < ApplicationController
+  include Garage::RestfulActions
+  skip_before_action :doorkeeper_authorize!, only: :create
+
+  def require_resources
+    @resources = Tourist.all
+  end
+
+  def require_resource
+    @resource = Tourist.find(params[:id])
+  end
+
+  def create_resource
+    @resources.create(tourist_params)
+  end
+
+  def update_resource
+    @resource.update_attributes!(tourist_params)
+  end
+
+  def destroy_resource
+    @resource.destroy!
+  end
+
+  private
+  def tourist_params
+    params.require(:tourist).permit(:name, :email, :password, :password_confirmation)
+  end
 end
