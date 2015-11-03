f4597ad07c54889a5829f47f91774c91e94f737d
山田　潤 <junyamada107@gmail.com>
2015-11-03 23:56:45 +0900

    add scope in order to permit access to user resource

--- config/initializers/garage.rb
+++ config/initializers/garage.rb
@@ -1,5 +1,11 @@
 Garage.configure {}
-Garage::TokenScope.configure {}
+
+Garage::TokenScope.configure do
+  register :public, desc: 'acessing publicly available data' do
+    access :read, Tourist
+    access :write, Tourist
+  end
+end
 
 Garage.configuration.strategy = Garage::Strategy::Doorkeeper
 
