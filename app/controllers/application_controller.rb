3780e67a282c97fe22028eb9a18647c97a0b9290
山田　潤 <junyamada107@gmail.com>
2015-11-03 23:52:18 +0900

    add resource owner method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  include Garage::ControllerHelper
+
+  def current_resource_owner
+    @current_resource_owner || Tourist.find(resource_owner_id) if resource_owner_id
+  end
 end
