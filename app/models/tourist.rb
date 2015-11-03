72ce4a8c24e32a0f4eed31b61174b578610d1590
山田　潤 <junyamada107@gmail.com>
2015-11-04 00:02:30 +0900

    init tourist model setting for garage

--- app/models/tourist.rb
+++ app/models/tourist.rb
@@ -1,6 +1,21 @@
 class Tourist < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  include Garage::Representer
+  include Garage::Authorizable
+
+  property :id
+  property :name
+  property :email
+
+  def self.build_permissions(perms, other, target)
+    perms.permits! :read
+    perms.permits! :write
+  end
+
+  def build_permissions(perms, other)
+    perms.permits! :read
+    perms.permits! :write if self == other
+  end
 end
