d70ed70cf37b1b632b26674655b2f09270fa8fa8
山田　潤 <junyamada107@gmail.com>
2015-11-04 02:39:44 +0900

    add devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   use_doorkeeper
-  devise_for :tourists
+  devise_for :tourists, :controllers => {
+   :registrations => 'users/registrations',
+   :sessions => 'users/sessions'
+  }
   constraints(format: :json) do
     resources :tourists
   end
