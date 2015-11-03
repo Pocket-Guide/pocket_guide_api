9f472fa608926cec6ef7d34b69d5ba5887170a45
山田　潤 <junyamada107@gmail.com>
2015-11-03 23:55:11 +0900

    setting garage.rb

--- config/initializers/garage.rb
+++ config/initializers/garage.rb
@@ -1,2 +1,16 @@
 Garage.configure {}
+Garage::TokenScope.configure {}
+
 Garage.configuration.strategy = Garage::Strategy::Doorkeeper
+
+Doorkeeper.configure do
+  orm :active_record
+  default_scopes :public
+  optional_scopes(*Garage::TokenScope.optional_scopes)
+
+  resource_owner_from_credentials do |routes|
+    Tourist.authenticate(params[:name], params[:password])
+  end
+end
+
+Doorkeeper.configuration.token_grant_types << "password"
