d49a38a1bb50ec6426cb2f0a257c5d2b3d0799bd
山田　潤 <junyamada107@gmail.com>
2015-12-11 00:50:01 +0900

    change parameter

--- app/controllers/current_tourist/plans_controller.rb
+++ app/controllers/current_tourist/plans_controller.rb
@@ -10,7 +10,7 @@ class CurrentTourist::PlansController < ApplicationController
 
   private
   def tour_params
-    params.permit(:content, :tourist_id)
+    params.permit(:content, :tourist_id, answers_attributes: [:question_id, :plan_id, :choice_id])
   end
 
   def require_resources
