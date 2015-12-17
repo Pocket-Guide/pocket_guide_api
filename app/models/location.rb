95d7bbf7033930881d411a87ac7decab8b8149f9
山田　潤 <junyamada107@gmail.com>
2015-12-17 17:27:13 +0900

    fixed method

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,9 @@ class Location < ActiveRecord::Base
   include Garage::Representer
   include Garage::Authorizable
 
+  attr_reader :probability
+
+  has_many :plan_locations
   has_many :plans, through: :plan_locations
   has_many :captured_images
 
@@ -21,5 +24,63 @@ class Location < ActiveRecord::Base
   def build_permissions(perms, other)
     perms.permits! :read
     perms.permits! :write if self == other
+  end
+
+  def self.classify_location
+
+  end
+
+  def self.conditional_probability
+    @location = Location.first
+    answers = Plan.find(30).answers
+    plans = []
+    @location.plans.each do |plan|
+      size = 0
+      plan.answers.each do |answer|
+        answers.each do |_answer|
+          if answer.choice_id == _answer.choice_id
+            size += 1
+          end
+        end
+      end
+      if size == plan.answers.count
+        plans << plan
+      end
+    end
+    @plan_count = @location.plans.count
+    probability = (plans.count.to_f / @plan_count.to_f)
+    return probability
+  end
+
+  def self.probability_x
+    answers = Plan.find(30).answers
+    plans = []
+    Plan.all.each do |plan|
+      size = 0
+      plan.answers.each do |answer|
+        answers.each do |_answer|
+          if answer.choice_id == _answer.choice_id
+            size += 1
+          end
+        end
+      end
+      if size == plan.answers.count
+        plans << plan
+      end
+    end
+    probability = plans.count.to_f / Plan.all.count.to_f
+    return probability
+
+  end
+
+  def self.probability_y
+    @location = Location.first
+    probability = @location.plans.count.to_f / Plan.all.count
+    return probability
+  end
+
+  def self.bayes_theorem
+    probability = conditional_probability * probability_y / probability_x
+    return probability
   end
 end
